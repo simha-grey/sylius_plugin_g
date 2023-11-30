c6ed3401e0bf24095122f2e5a82494dd8bdb1da6
roman <r.filipovskyy.synergetica@gmail.com>
2023-11-30 12:51:42 +0200

    product enable/disable 4

--- src/Repository/ProductWithStockRepository.php
+++ src/Repository/ProductWithStockRepository.php
@@ -10,17 +10,17 @@ class ProductWithStockRepository extends BaseProductRepository implements Produc
     public const PAGINATOR_PER_PAGE = 5;
     public function getProductWithStockPaginator(int $offset, int $status): Paginator
     {
-        $query= $this->createQueryBuilder('p')
-            ->addSelect('ps')
+        $qb= $this->createQueryBuilder('p');
+        $qb->addSelect('ps')
             ->leftJoin('Roma\SyliusProductVariantPlugin\Entity\ProductStock', 'ps','WITH', 'p.id = ps.product')
             ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->getQuery()
             ->setFirstResult($offset);
 
         if(!empty($status)){
-            $query->setParameter('stockStatus', $status)->where('ps.stockStatus > :stockStatus');
+            $qb->where('ps.stockStatus = :stockStatus')->setParameter('stockStatus', $status);
         }
 
-        return new Paginator($query);
+        return new Paginator($qb);
     }
 }
